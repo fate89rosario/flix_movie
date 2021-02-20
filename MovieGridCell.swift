6635b5758f16b70a4bdf98028f6ff2a958fc885c
fate89rosario <56460772+fate89rosario@users.noreply.github.com>
2021-02-20 06:08:29 +0500

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix Movie
+//
+//  Created by Fate  on 2/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
